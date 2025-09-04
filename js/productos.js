a8aab10abec4e4c1404c082146f587edb580a020
marimilhas <marimilhas@gmail.com>
2025-09-04 17:47:53 -0300

    Cambié productos.js para que consuma el json

--- js/productos.js
+++ js/productos.js
@@ -1,43 +1,20 @@
-const productos = [
-    {
-        nombre: "Aparador Uspallata",
-        descripcion: "Aparador de seis puertas en nogal sostenible con tiradores de latón, que combina funcionalidad y elegancia atemporal.",
-        imagen: "images/Aparador Uspallata.png",
-        link: "#"
-    },
-    {
-        nombre: "Mesa de Centro Araucaria",
-        descripcion: "Mesa de centro minimalista con sobre circular de mármol y base de madera de nogal, ideal como punto focal en salas contemporáneas.",
-        imagen: "images/Mesa de Centro Araucaria.png",
-        link: "#"
-    },
-    
-    {
-        nombre: "Biblioteca Recoleta",
-        descripcion: "Sistema modular de estantes con estructura de acero Sage Green y repisas de roble claro, versátil y elegante para cualquier espacio.",
-        imagen: "images/Biblioteca Recoleta.png",
-        link: "#"
-    }
-    // Agregá más productos acá
-];
+const productosContainer = document.querySelector('.productos-grid');
 
-const contenedorProductos = document.querySelector('.productos-grid');
+fetch('productos.json')
+    .then(response => response.json())
+    .then(productos => {
+        productos.forEach(producto => {
+            const articulo = document.createElement('article');
+            articulo.classList.add('producto');
 
-function cargarProductos() {
-    contenedorProductos.innerHTML = ""; // limpiar contenido
-    productos.forEach(producto => {
-        const card = document.createElement('article');
-        card.classList.add('producto');
+            articulo.innerHTML = `
+                <a href="#">
+                    <img src="${producto.imagen}" alt="${producto.nombre}">
+                    <h2>${producto.product_name}</h2>
+                </a>
+            `;
 
-        card.innerHTML = `
-            <a href="">
-                <img src="${producto.imagen}" alt="${producto.nombre}">
-                <h2>${producto.nombre}</h3>
-                <p>${producto.descripcion}</p>
-            </a>
-        `;
-        contenedorProductos.appendChild(card);
-    });
-}
-
-cargarProductos();
+            productosContainer.appendChild(articulo);
+        });
+    })
+    .catch(error => console.error('Error cargando los productos:', error));
