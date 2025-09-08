03366c4a1eba86dd412a7d5b151390dc35621ab4
marimilhas <marimilhas@gmail.com>
2025-09-07 23:32:21 -0300

    inicio cambiado a raiz

--- carrito/carrito.js
+++ carrito/carrito.js
@@ -146,8 +146,8 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
 if (hamburger && menu) {
-    hamburger.addEventListener("click", () => {
-        menu.classList.toggle("active");
-    });
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
 }
   
