27d28fb87680d63380c77f0ce8ac8e059b7946cc
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2025-10-03 23:32:15 -0300

    fix - typo en la ruta donde se debe aplicar el proxy de desarrollo

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: VITE_API_HOST,
         changeOrigin: true,
       },
