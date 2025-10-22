3860fd5d5ecbbfb3770c1ee7c3330d864ee53ce5
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2025-10-22 10:37:16 -0300

    feat: Agrego manejo de errores al procesar el multipart/form-data

--- backend/middleware/imageUploader.js
+++ backend/middleware/imageUploader.js
@@ -1,33 +1,41 @@
-import multer from "multer";
-import { APP_MODE } from "../config.js";
+import multer from "multer"
+import { APP_MODE } from "../config.js"
+import { AppError } from "../errors/error.js"
 
 // Dependiendo de si estamos en dev o prod, guardo las imagenes donde el clente pueda accederlas
-let destination = "../client/public/assets/";
+let destination = "../client/public/assets/"
 if (APP_MODE === "prod") {
-  destination = "../dist/assets/";
+	destination = "../dist/assets/"
 }
 
 const storage = multer.diskStorage({
-  
-  destination: function (req, file, cb) {
-    cb(null, destination);
-  },
-  filename: function (req, file, cb) {
-    const timestamp = Date.now()
-    const productName = req.body.product_name;
-    const extension = file.originalname.split(".").slice(-1);
-    cb(null, `${timestamp}-${productName}.${extension}`);
-  },
-});
+	destination: function (req, file, cb) {
+		cb(null, destination)
+	},
+	filename: function (req, file, cb) {
+		const timestamp = Date.now()
+		const productName = req.body.product_name
+		const extension = file.originalname.split(".").slice(-1)
+		cb(null, `${timestamp}-${productName}.${extension}`)
+	},
+})
 
-function filterMimeTypes (req, file, cb) {
-  if (!file.mimetype.startsWith("image/")) {
-    cb(new Error("Invalid file type"), false);
-  }
-  cb(null, true);
+function filterMimeTypes(req, file, cb) {
+	if (!file.mimetype.startsWith("image/")) {
+		cb(new AppError("Invalid file type", 400), false)
+	}
+	cb(null, true)
 }
 
-export const imageUploader = multer({
-  storage: storage,
-  fileFilter: filterMimeTypes,
-});
+const imageUploader = multer({
+	storage: storage,
+	fileFilter: filterMimeTypes,
+}).single("imagen")
+
+export function handlerImageUploader(req, res, next) {
+	// Esto gestiona la carga de la imagen al servidor
+	// Además, parsea el form-data para que los campos puedan ser leidos desde req.body
+  imageUploader(req, res, (err) => {
+		err ? next(new AppError(err.message, 400)) : next()
+	})
+}
