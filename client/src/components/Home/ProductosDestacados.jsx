e2f7af3d35bc935cb5d4d8d9d27490865bdc48fe
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2025-10-29 11:00:30 -0300

    refactor - ProductosDestacados usa ProductCard + estilos

--- client/src/components/Home/ProductosDestacados.jsx
+++ client/src/components/Home/ProductosDestacados.jsx
@@ -1,9 +1,11 @@
 import useFetch from "hooks/useFetch"
-import { Link } from "react-router"
+import { useCartContext } from "context/carrito"
 import { Suspense } from "react"
+import ProductCard from "components/ui/ProductCard"
 
 export default function ProductosDestacados() {
 	const { data, loading, error } = useFetch("/api/productos?r=true")
+	const { addProduct } = useCartContext()
 
 	if (error) {
 		console.log(error)
@@ -12,39 +14,27 @@ export default function ProductosDestacados() {
 
 	return (
 		<Suspense>
-			<section className='section-box'>
-				<h2>Productos Destacados</h2>
-				<div className='productos-container'>
-					{loading ? <p>cargando</p> : <ShowProd products={data} />}
+			<section className='flex flex-col justify-center items-center'>
+				<h3 className='text-4xl font-semibold  font-title tracking-wider text-primary p-5 text-center'>
+					Selección para ti
+				</h3>
+				<p className="pb-10 text-pretty font-body font-light text-center text-home-100/60">
+					Cada pieza pensada para darle vida a tu hogar.
+				</p>
+				<div className='flex justify-center items-center flex-wrap gap-5'>
+					{loading ? (
+						<div>Cargando</div>
+					) : (
+						data.slice(0, 3).map((p) => (
+							<ProductCard
+								producto={p}
+								key={p.id}
+								addToCart={() => addProduct(p)}
+							/>
+						))
+					)}
 				</div>
 			</section>
 		</Suspense>
-	)
-}
-
-function ShowProd({ products }) {
-	const productsToShow = products?.slice(0, 3) // Mostrar solo los primeros 3 productos
-	const listProd = productsToShow?.map((p) => (
-		<ProdCard
-			p={p}
-			key={p.id}
-		/>
-	))
-	return listProd
-}
-
-function ProdCard({ p }) {
-	return (
-		<>
-			<article className='producto'>
-				<Link to={"/producto/" + p.id}>
-					<img
-						src={`/assets/${p.imagen}`}
-						alt={p.product_name}
-					/>
-					<h2> {p.product_name} </h2>
-				</Link>
-			</article>
-		</>
 	)
 }
