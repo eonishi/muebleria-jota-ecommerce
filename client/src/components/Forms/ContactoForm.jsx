2b5aec4c16bc74e50e07716b42e45a7408de38e2
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2025-11-04 16:52:17 -0300

    refacto - Estilos del formulario de contacto

--- client/src/components/Forms/ContactoForm.jsx
+++ client/src/components/Forms/ContactoForm.jsx
@@ -5,9 +5,14 @@ import { ContactoSchema } from "schemas"
 import { useEffect } from "react"
 
 export default function ContactoForm() {
-	const { control, reset, handleSubmit, formState: { errors, isSubmitSuccessful }, } = useForm({
+	const {
+		control,
+		reset,
+		handleSubmit,
+		formState: { errors, isSubmitSuccessful, isSubmitting },
+	} = useForm({
 		resolver: zodResolver(ContactoSchema),
-		mode: 'noBlur',
+		mode: "noBlur",
 		defaultValues: {
 			username: "",
 			email: "",
@@ -27,47 +32,83 @@ export default function ContactoForm() {
 		if (isSubmitSuccessful) {
 			reset()
 		}
-	 },[reset, isSubmitSuccessful])
+	}, [reset, isSubmitSuccessful])
 
 	return (
-		<section className='section-box contacto'>
-			<h2>Contacto</h2>
+		<section className='md:mt-30 flex flex-col items-center justify-center relative'>
 
-			<div className='contacto-container'>
-				<form
-					onSubmit={handleSubmit(onSubmit)}
-					id='formularioDeContacto'
+			<h2 className='text-4xl font-semibold  font-title text-primary p-5 text-center text-balance'>
+				Ponte en contacto con nosotros!
+			</h2>
+			<p className='pb-10 text-pretty font-body font-light text-center text-home-100/60 max-w-250'>
+				Para obtener más información sobre nuestros productos y servicios, no
+				dude en enviarnos un correo electrónico.
+				<br />
+				Estamos encantados de poder ayudarte 💕.
+			</p>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='grid grid-cols-1 md:grid-cols-2 gap-5 p-5 max-w-250 w-full font-body'
+			>
+				<InputForm
+					name='username'
+					control={control}
+					label='Nombre'
+					type='text'
+					error={errors.username}
+					placeholder='Herman Miller'
+					className='col-span-1'
+				/>
+				<InputForm
+					name='email'
+					control={control}
+					label='Email'
+					type='email'
+					error={errors.email}
+					placeholder='miller@furniture.com'
+					className='md:col-span-1'
+				/>
+				<InputForm
+					name='phone'
+					control={control}
+					label='Teléfono'
+					type='tel'
+					error={errors.phone}
+					placeholder='+54 011 1234-5678'
+					className='md:col-span-1'
+				/>
+
+				<InputForm
+					name='subject'
+					control={control}
+					label='Asunto'
+					type='text'
+					error={errors.subject}
+					placeholder='Servicios personalizados'
+					className='md:col-span-2'
+				/>
+
+				<InputForm
+					name='message'
+					control={control}
+					label='Mensaje'
+					type='text'
+					error={errors.message}
+					as='textarea'
+					placeholder='¡Queremos trabajar contigo! 🧉👋'
+					className='md:col-span-2'
+				/>
+
+				<button
+					type='submit'
+					className={`p-5 bg-detail active:bg-detail-200 hover:scale-101 active:scale-100 transition-transform
+						md:col-end-3 text-neutral-50 focus:outline focus:outline-primary ${
+							isSubmitting ? "cursor-progress" : "cursor-pointer"
+						}`}
 				>
-					<InputForm
-						name='username'
-						control={control}
-						label='Nombre'
-						type='text'
-						error={errors.username}
-					/>
-					<InputForm
-						name='email'
-						control={control}
-						label='Email'
-						type='email'
-						error={errors.email}
-					/>
-					<InputForm
-						name='message'
-						control={control}
-						label='Mensaje'
-						type='text'
-						error={errors.message}
-						as='textarea'
-					/>
-					<button
-						type='submit'
-						className='btn btn-contacto'
-					>
-						Enviar
-					</button>
-				</form>
-			</div>
+					Enviar
+				</button>
+			</form>
 		</section>
 	)
 }
